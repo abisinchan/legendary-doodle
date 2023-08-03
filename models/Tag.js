d15ea1e60bd845a269feaee6cba2a4419ee42c8f
AE SChan <abigailsinchan@gmail.com>
2023-08-03 16:32:50 -0400

    fixed columns for models and added associations

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,20 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
